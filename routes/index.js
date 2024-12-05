64f6ad7aee08b75dd0bbef0b48bd28173d8eebea
Sthornburg99 <skythorn99@gmail.com>
2024-12-05 13:23:05 -0500

    Update index.js
    Adding querying for student, still need more for student, faculty and registrar

--- routes/index.js
+++ routes/index.js
@@ -9,4 +9,32 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.post('/student', function(req, res, next) {
+  // Do appropriate tweaking here before calling res.render
+  console.log(req.body);
+
+  currentQuery = 'select CourseNo, OffTerm, OffYear, FacFirstName, FacLastName, EnrGrade '
+                + 'from (Enrollment natural join Offering) '
+                + 'left join Faculty on Offering.FacSSN = Faculty.FacSSN '
+                + "where StdSSN = '" + req.body.id + "';"
+  console.log(currentQuery);
+  req.app.locals.db.all(currentQuery, [], (err, rows) => {
+    if (err) {
+      throw err;
+    }
+    req.app.locals.enrolledCourses = rows;
+
+    // Do the other query here, and call res.render from its handler
+
+    // Until the other query is implemented, call res.render here
+    res.render('student', { role: req.body.role,
+                            userID: req.body.id,
+                            formdata: req.body,
+                            enrolledCourses: req.app.locals.enrolledCourses
+    });
+  });
+});
+
+// Similar for faculty and registrar
+
 module.exports = router;
