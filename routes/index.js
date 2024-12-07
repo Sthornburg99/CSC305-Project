345d28b4d747f730567899c16e36bece1e8280c6
William Lopez <williamelopez2021@gmail.com>
2024-12-06 23:14:45 -0400

    awesome sauce

--- routes/index.js
+++ routes/index.js
@@ -9,6 +9,9 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+
+
+
 router.post('/student', function(req, res, next) {
   // Do appropriate tweaking here before calling res.render
   console.log(req.body);
@@ -16,7 +19,7 @@ router.post('/student', function(req, res, next) {
   currentQuery = 'select CourseNo, OffTerm, OffYear, FacFirstName, FacLastName, EnrGrade '
                 + 'from (Enrollment natural join Offering) '
                 + 'left join Faculty on Offering.FacSSN = Faculty.FacSSN '
-                + "where StdSSN = '" + req.body.id + "';"
+                + 'where OffTerm = "FALL" and OffYear = "2024"'
   console.log(currentQuery);
   req.app.locals.db.all(currentQuery, [], (err, rows) => {
     if (err) {
@@ -25,25 +28,61 @@ router.post('/student', function(req, res, next) {
     req.app.locals.enrolledCourses = rows;
 
     // Do the other query here, and call res.render from its handler
-    availableQuery = 'select CourseNo, FacFirstName, FacLastName, OffLocation, OffTime, OffDays, OfferNo '
-                    + 'from (Enrollement natural join Offering '
+    availableQuery = 'select CourseNo, FacFirstName, FacLastName, OffLocation, OffTime, OffDays '
+                    + 'from Offering ' 
                     + 'left join Faculty on Offering.FacSSN = Faculty.FacSSN '
-                    + "where StdSSN = '" + req.body.id + "';"
+                    + 'where OffTerm = "WINTER"'
     console.log(availableQuery);
     req.app.locals.db.all(availableQuery, [], (err, rows) => {
       if (err) {
         throw err;
       }
-      req.app.locals.enrolledCourses = rows;
+      req.app.locals.availableCourses = rows;
+      /*let addQuery = 'select OfferNo, CourseNo, FacFirstName, FacLastName, OffLocation, OffTime, OffDays '
+                      + 'from Offering '
+                      + 'left join Faculty on Offering.FacSSN = Faculty.FacSSN '
+                      + 'where OffTerm = "WINTER" and OffYear ="2025"'
+    req.app.locals.db.all(addQuery, [], (err, rows) => {
+      if (err){
+        throw err;
+      }
+      req.app.locals.addingCourses=rows;
+     })*/
     // Until the other query is implemented, call res.render here
     res.render('student', { role: req.body.role,
                             userID: req.body.id,
                             formdata: req.body,
-                            enrolledCourses: req.app.locals.enrolledCourses
+                            enrolledCourses: req.app.locals.enrolledCourses,
+                            availableCourses: req.app.locals.availableCourses
+                            //addingCourses: req.app.locals.addingCourses
     });
   });
 });
 });
+
+router.post('/enrolled', function(req, res, next) {
+  if (req.app.locals.formdata) {
+    let addQuery = 'select OfferNo, CourseNo, FacFirstName, FacLastName, OffLocation, OffTime, OffDays '
+                  + 'from Offering '
+                  + 'left join Faculty on Offering.FacSSN = Faculty.FacSSN '
+                  + 'where OffTerm = "WINTER" and OffYear ="2025"'
+    req.app.locals.db.all(addQuery, [], (err, rows) => {
+      if (err){
+        throw err;
+      }
+      req.app.locals.addingCourses=rows;
+    })
+
+  res.render('student', { role: req.body.role,
+    userID: req.body.id,
+    formdata: req.body,
+    enrolledCourses: req.app.locals.enrolledCourses,
+    availableCourses: req.app.locals.availableCourses,
+    addingCourses: req.app.locals.addingCourses
+  });
+  };
+});
+
 router.post('/faculty', function(req, res, next) {
   console.log(req.body);
 
@@ -57,15 +96,95 @@ router.post('/faculty', function(req, res, next) {
       throw err;
     }
     req.app.locals.currentCourses = rows;
+  
+    studentsQuerry = 'select StdFirstName, StdLastName, StdSSN, EnrGrade '
+                    + 'from (Student natural join Enrollment) '
+                    + 'left join Offering on Enrollment.OfferNo = Offering.OfferNo '
+                    + "where FacSSN = '" + req.body.id +"';"
+
+    console.log(studentsQuerry)
+    req.app.locals.db.all(studentsQuerry, [], (err, rows) => {
+      if (err) {
+        throw err;
+      }
+      req.app.locals.currentStudent = rows;
+    
 
     res.render('faculty', { role: req.body.role,
                             userID: req.body.id,
                             formdata: req.body,
-                            currentCourses: req.app.locals.currentCourses
-                          });
+                            currentCourses: req.app.locals.currentCourses,
+                            currentStudent: req.app.locals.currentStudent
+      });
+    });                    
   });
 });
 
-// Similar for faculty and registrar
+router.post('/editgrade', function(req, res, next) {
 
+
+  res.render('faculty', { role: req.body.role,
+    userID: req.body.id,
+    formdata: req.body,
+    currentCourses: req.app.locals.currentCourses,
+    currentStudent: req.app.locals.currentStudent
+  });
+});
+
+router.post('/registrar', function(req, res, next) {
+  console.log(req.body);
+
+  coursesQuerry = 'select OfferNo, CourseNo, FacFirstName, FacLastName, OffLocation, OffTime, OffDays '
+                + 'from Offering '
+                + 'join Faculty on Offering.FacSSN = Faculty.FacSSN'
+
+  console.log(coursesQuerry)
+  req.app.locals.db.all(coursesQuerry, [], (err, rows) => {
+    if (err) {
+      throw err;
+    }
+    req.app.locals.openCourses = rows;
+
+  changesQuerry = 'select FacFirstName, FacLastName, OffTime, OffDays, OffLocation '
+                + 'from Offering '
+                + 'join Faculty on Offering.FacSSN = Faculty.FacSSN'
+
+  console.log(changesQuerry)
+  req.app.locals.db.all(changesQuerry, [], (err, rows) => {
+    if (err) {
+      throw err;
+    }
+  req.app.locals.changeCourses = rows;
+  
+    
+
+    res.render('registrar', { role: req.body.role,
+                            userID: req.body.id,
+                            formdata: req.body,
+                            openCourses: req.app.locals.openCourses,
+                            changeCourses: req.app.locals.changeCourses
+
+      });
+    });                    
+  });
+});
+router.post('/editCourses', function(req, res, next) {
+
+  res.render('registrar', { role: req.body.role,
+    userID: req.body.id,
+    formdata: req.body,
+    openCourses: req.app.locals.openCourses,
+    changeCourses: req.app.locals.changeCourses
+  });
+});
+
+router.post('/addCourse', function(req, res, next) {
+
+  res.render('registrar', { role: req.body.role,
+    userID: req.body.id,
+    formdata: req.body,
+    openCourses: req.app.locals.openCourses,
+    changeCourses: req.app.locals.changeCourses
+  });
+});
 module.exports = router;
