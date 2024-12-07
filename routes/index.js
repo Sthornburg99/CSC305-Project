b88fc47702ff70201e7c8880d0b09d331631ccb8
William Lopez <williamelopez2021@gmail.com>
2024-12-06 23:11:52 -0400

    Pushing P

--- routes/index.js
+++ routes/index.js
@@ -25,13 +25,44 @@ router.post('/student', function(req, res, next) {
     req.app.locals.enrolledCourses = rows;
 
     // Do the other query here, and call res.render from its handler
-
+    availableQuery = 'select CourseNo, FacFirstName, FacLastName, OffLocation, OffTime, OffDays, OfferNo '
+                    + 'from (Enrollement natural join Offering '
+                    + 'left join Faculty on Offering.FacSSN = Faculty.FacSSN '
+                    + "where StdSSN = '" + req.body.id + "';"
+    console.log(availableQuery);
+    req.app.locals.db.all(availableQuery, [], (err, rows) => {
+      if (err) {
+        throw err;
+      }
+      req.app.locals.enrolledCourses = rows;
     // Until the other query is implemented, call res.render here
     res.render('student', { role: req.body.role,
                             userID: req.body.id,
                             formdata: req.body,
                             enrolledCourses: req.app.locals.enrolledCourses
     });
+  });
+});
+});
+router.post('/faculty', function(req, res, next) {
+  console.log(req.body);
+
+  facultyQuerry = 'select CourseNo, OffTerm, OffYear '
+                + 'from Offering '
+                + "where FacSSN = '" + req.body.id + "';"
+
+  console.log(facultyQuerry)
+  req.app.locals.db.all(facultyQuerry, [], (err, rows) => {
+    if (err) {
+      throw err;
+    }
+    req.app.locals.currentCourses = rows;
+
+    res.render('faculty', { role: req.body.role,
+                            userID: req.body.id,
+                            formdata: req.body,
+                            currentCourses: req.app.locals.currentCourses
+                          });
   });
 });
 
